11066f030e8b510e415e9d5eb8fb95d1d2200354
Next Friday Developer <nextfriday.developer@gmail.com>
2025-09-09 05:27:46 +0700

    chore(release): bump version to 1.0.2 (#9)
    * feat(prefer-import-type): enhance rule to exclude runtime imports
    
    * ci(release): fix fetch step to avoid checkout branch conflict
    
    * ci(release): fix workflow fetch bug in release.yml
    
    ---------
    
    Co-authored-by: Joe Takara <anuchit.boonsom@gmail.com>

--- src/rules/__tests__/prefer-import-type.test.ts
+++ src/rules/__tests__/prefer-import-type.test.ts
@@ -24,8 +24,19 @@ describe("prefer-import-type", () => {
 
   ruleTester.run("prefer-import-type", preferImportType, {
     valid: [
+      'import "../components/Button.module.css";',
+      'import "./globals.css";',
+      'import "./styles.scss";',
       'import * as utils from "./utils";',
+      'import Image from "next/image";',
+      'import Link from "next/link";',
       'import React from "react";',
+      'import React, { useState } from "react";',
+      'import data from "./data.json";',
+      'import dynamic from "next/dynamic";',
+      'import icon from "./icon.svg";',
+      'import logo from "./logo.png";',
+      'import styled from "styled-components";',
       'import type { Component } from "react";',
       'import type { RuleContext } from "@typescript-eslint/utils";',
       'import type { TSESTree } from "@typescript-eslint/utils";',
@@ -33,6 +44,8 @@ describe("prefer-import-type", () => {
       'import { ESLintUtils, type TSESTree } from "@typescript-eslint/utils";',
       'import { Geist, Geist_Mono } from "next/font/google";',
       'import { Inter } from "next/font/google";',
+      'import { createRoot } from "react-dom/client";',
+      'import { css } from "@emotion/react";',
       'import { useState, useEffect } from "react";',
     ],
     invalid: [
